c7c59c7508c7fec0b7923efd119530f22021a244
izumichan16 <c2403974446@gmail.com>
2022-05-27 11:42:09 +0800

    添加了注释和编写README

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Chaoxing_comment
+# 基于Python selenium的学习通自动评论
+
+## 如何使用
+1. 你需要git clone本项目，或者下载zip包
+![](https://cos.izumichan16.cn/img/20220527112831.png)
+2. 在pycharm或任意可以运行python代码的编辑器中打开本项目
+3. 请确认你的python版本在3.x,本项目未做任何低于3.10版本的测试
+4. 请导入`selenium`包
+```shell
+pip install selenium
+```
+4. 你需要下载[对应的selenium浏览器驱动](https://www.selenium.dev/zh-cn/documentation/webdriver/getting_started/install_drivers/)，将其放置到`driver`文件夹中  
+
+![快速参考](https://cos.izumichan16.cn/img/20220527110545.png)  
+浏览器的版本您可以在浏览器设置中查看
+![版本查看](https://cos.izumichan16.cn/img/20220527111444.png)  
+点击下载链接后，请选择对应的版本
+![版本选择](https://cos.izumichan16.cn/img/20220527110618.png)  
+选择合适的平台下载解压
+![下载解压](https://cos.izumichan16.cn/img/20220527110629.png)  
+
+将解压后的文件替换本项目`driver`文件夹中的`chromedriver`（如果您也是使用Linux上最新版的chrome则无需更换）
+
+4. 请把`comment.py`文件中的`name_input`和`pwd_input`替换成你的用户名和密码
+
+5. 如果你不是我班同学，请把`comment.py`文件中的url替换成你需要评论的主网页
+![如图操作](https://cos.izumichan16.cn/img/20220527113245.png)
+
+6. 最后您可以直接运行本程序
+![](https://cos.izumichan16.cn/img/20220527113823.png)
+
+## 最后
+有任何问题可以提交issue或微信联系我本人（仅限同学
--- comment.py
+++ comment.py
@@ -1,39 +1,53 @@
 import csv
 import time
-
-import requests
 from selenium import webdriver
 
-# 引入driver
-chromedriver = "driver/chromedriver"
-browser = webdriver.Chrome(chromedriver)
 
-comment_url = "https://mooc2-ans.chaoxing.com/mycourse/stu?courseid=223205951&clazzid=52049524&cpi=161306048&enc" \
-              "=89cde758bfa7ce4626a1d114da6bad1e&t=1653612717461&pageHeader=2 "
+def action():
+    # 引入driver，这里替换成你所使用的driver版本
+    chromedriver = "driver/chromedriver"
+    browser = webdriver.Chrome(chromedriver)
 
-browser.get(comment_url)
-time.sleep(2)
+    # 评论课程的链接
+    comment_url = "https://mooc2-ans.chaoxing.com/mycourse/stu?courseid=223205951&clazzid=52049524&cpi=161306048&enc" \
+                  "=89cde758bfa7ce4626a1d114da6bad1e&t=1653612717461&pageHeader=2 "
 
-name_input = browser.find_element_by_css_selector("#phone")
-pwd_input = browser.find_element_by_css_selector("#pwd")
-name_input.send_keys('15779703454')
-pwd_input.send_keys('izumiChan0816')
+    browser.get(comment_url)
+    time.sleep(2)
 
-browser.find_element_by_css_selector("#loginBtn").click()
-time.sleep(3)
-# 打开网页
+    name_input = browser.find_element_by_css_selector("#phone")
+    pwd_input = browser.find_element_by_css_selector("#pwd")
+    # 替换成你的用户名
+    name_input.send_keys('123456789')
+    # 替换成你的密码
+    pwd_input.send_keys('123456asdfga')
+
+    browser.find_element_by_css_selector("#loginBtn").click()
+    time.sleep(3)
+    # 打开网页
+
+    with open('resources/100.csv', encoding="utf-8") as csvfile:
+        # 读取评论文件，一行一句话
+        reader = csv.reader(csvfile)
+        for row in reader:
+            # 网页结构是多个iframe的嵌套，这里先进入第一层iframe
+            browser.switch_to.frame(0)
+            # 进入了第一层以后，再进入第一层里的第一层（也就是主网页的第二层）
+            browser.switch_to.frame(0)
+            # 查找评论区
+            browser.find_element_by_css_selector('body').clear()
+            browser.find_element_by_css_selector('body').send_keys(row)
+            time.sleep(1)
+            # 回到主网页
+            browser.switch_to.default_content()
+            # 因为提交按钮在主网页的第一层，所以需要重新进入一次
+            browser.switch_to.frame(0)
+            # 使用xpath定位提交按钮
+            browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[3]/div[4]/div[2]').click()
+            # 进入下一次评论前需要回到主网页，否则会保留状态导致无法寻址
+            browser.switch_to.default_content()
+            time.sleep(5)
 
 
-with open('resources/100.csv', encoding="utf-8") as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        browser.switch_to.frame(0)
-        browser.switch_to.frame(0)
-        browser.find_element_by_css_selector('body').clear()
-        browser.find_element_by_css_selector('body').send_keys(row)
-        time.sleep(1)
-        browser.switch_to.default_content()
-        browser.switch_to.frame(0)
-        browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[3]/div[4]/div[2]').click()
-        browser.switch_to.default_content()
-        time.sleep(5)
+if __name__ == '__main__':
+    action()
